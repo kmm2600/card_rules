7325845e56a7730b8e9d99578d983c0f995039a4
kmm2600 <127270325+kmm2600@users.noreply.github.com>
2023-05-24 21:10:13 -0500

    Update streamlit_app.py
    formatting

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ df = df[df['Type'] == type_choice]
 card = sorted(list(df['Card'].drop_duplicates()))
 card_choice = st.selectbox('Filter on a card', card)
 df = df[df['Card'] == card_choice]
-df = df.drop(df.index[0])
 df = df.drop(['Type', 'Card'], axis=1)
+df = df.set_index('Description',inplace=True)
 display_df = df.transpose()
 st.table(display_df)
